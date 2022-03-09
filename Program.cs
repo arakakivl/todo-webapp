578e9bcc49c6a41b12265ac5bbe6577cc8363c8b
arakakiv <vvitor.bbarcelos@gmail.com>
2022-03-09 13:54:15 -0300

    Adicionando injection dependecy

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
+using ToDoApi.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
